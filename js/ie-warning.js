26bd366e28ad0e48e7024a3922f24a6f95ceb3e6
Keith Silgard <ksilgard@gmail.com>
2011-08-11 21:38:04 -0600

    Setting up a solid base that I'm pretty happy with.

--- js/ie-warning.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	//have to put this on click because IE sucks... should be on change ;D
-	$('#i-know, label[for=i-know]').click(function() {
-		$.get('/?hideIeWarning=1', function (data) {
-			//alert(data);
-			$('#ie-warning').slideUp('slow');
-		});
-	});
-});
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var Site = window.Site || {};
 (function($) {
 	//on load:
 	$(function() {
-		$('button').click(function(){$(this).closest('form').submit();});
+		
 	});	
 	
 })(jQuery);
