6f45e75137bea4c9d1695a2301d044c3d0b1e522
Keith Silgard <ksilgard@gmail.com>
2013-07-09 14:16:38 -0700

    Just need to commit something as a test

---
+++ js/projects/password-generator.js
@@ -0,0 +1,15 @@
+passtacular = {
+	likes: {
+		foods: ['pizza', 'pie', 'popcorn', 'chips', 'rice', 'poutine', 'tacos', 'toast', 'tofu', 'ketchup', 'chicken', 'beef', 'steak', 'crab', 'syrup', 'fish', 'pho', 'bread', 'pasta', 'meat', 'veggies', 'vegetables', 'italian', 'chinese'],
+		sports: ['hockey', 'baseball', 'football', 'basketball', 'soccer', 'tennis', 'golf', 'cricket', 'volleyball'],
+		movies: [],
+		celebrities: []
+	}, 
+	names: {
+		celebrities: ['mixalot']
+	},
+};
+passtacular.generate = function (name, like)
+{
+	
+};
